684da3454ac089b8ae25e7d04a8002d16bc18ab0
Caroline Cruz <cruz.caroline08@gmail.com>
2017-10-16 15:12:25 -0400

    Initial Commit

---
+++ ThreeCardMonteTests/ThreeCardMonteTests.swift
@@ -0,0 +1,36 @@
+//
+//  ThreeCardMonteTests.swift
+//  ThreeCardMonteTests
+//
+//  Created by Keshawn Swanston on 10/16/17.
+//  Copyright © 2017 Caroline Cruz. All rights reserved.
+//
+
+import XCTest
+@testable import ThreeCardMonte
+
+class ThreeCardMonteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
